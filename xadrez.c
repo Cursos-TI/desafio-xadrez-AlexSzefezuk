b907a425c3f6df851f4f1440d6bd10b76a25853a
Alex Szefezuk <92179905+AlexSzefezuk@users.noreply.github.com>
2025-03-16 15:24:32 +0000

    desafio aventureiro

--- xadrez.c
+++ xadrez.c
@@ -22,5 +22,16 @@ int main() {
         printf("Rainha: Esquerda\n");
     }
 
+    printf("---------------Iniciando movimentação do cavalo---------------\n");
+
+    for (int l = 1; l <= 1; l++) {
+        for(int m = 1; m <= 2; m++) {
+            printf("Cavalo: Cima\n");
+        }
+        printf("Cavalo: Direita\n");
+    }
+
+
     return 0;
 }
+
